f0518d05f1b03bea29b9a33bfe30b7f1c4381ce0
Juanan Ruiz <juanan@us.es>
2018-07-28 23:19:24 +0200

    Aplicando framework CSS Skeleton

--- src/factura_crear.php
+++ src/factura_crear.php
@@ -3,22 +3,38 @@ global $dbh;
 
 $query = $dbh->prepare(
     "SELECT CONCAT('F-',LPAD(SUBSTR(codigo,3,4)+1,4,'0'), '/', 
-                YEAR(NOW())) AS codigo 
-                FROM factura WHERE year(fecha_emision) = YEAR(NOW())
-              UNION
-                SELECT CONCAT('F-0001', '/', YEAR(NOW())) AS codigo
-              ORDER BY codigo DESC LIMIT 1;;
-    ");
+            YEAR(NOW())) AS codigo 
+            FROM factura WHERE year(fecha_emision) = YEAR(NOW())
+          UNION
+            SELECT CONCAT('F-0001', '/', YEAR(NOW())) AS codigo
+          ORDER BY codigo DESC LIMIT 1;;
+");
 // Establece la forma de devolver los resultados, en este caso devolverá un array asociativo
 $query->setFetchMode(PDO::FETCH_ASSOC);
 $query->execute();
+?>
 
-echo "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'></head><body>";
-while ($row = $query->fetch()){
-    echo '<form method="post" action="index.php?p=factura_grabar">';
-    echo '<input type="text" name="codigo" value="' . $row["codigo"] . '">';
-    echo '<input type="date" name="fecha_emision">';
-    echo '<input type="submit" value="Grabar factura">';
-    echo '</form>';
-}
-echo "</body></html>";
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Generar código autonumérico con reinicio anual</title>
+    <link rel='stylesheet' href='css/normalize.css'>
+    <link rel='stylesheet' href='css/skeleton.css'>
+    <link rel='stylesheet' href='css/custom.css'>
+</head>
+<body>
+<div class='container'>
+    <section class='header'><h3>Generar código autonumérico con reinicio anual</h3></section>
+    <?php
+    while ($row = $query->fetch()) {
+        echo '<form method="post" action="index.php?p=factura_grabar">';
+        echo '<input type="text" name="codigo" value="' . $row["codigo"] . '">';
+        echo '<input type="date" name="fecha_emision">';
+        echo '<input type="submit" value="Grabar factura">';
+        echo '</form>';
+    }
+    ?>
+</div>
+</body>
+</html>";
--- src/inicio.php
+++ src/inicio.php
@@ -1,18 +1,31 @@
-<?php
-global $dbh;
+<?php global $dbh; ?>
 
-echo "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'></head><body>";
-echo "<h1>Facturación</h1>";
-echo "<p><a href='index.php?p=factura_crear'>Crear factura</a></p>";
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Generar código autonumérico con reinicio anual</title>
+    <link rel='stylesheet' href='css/normalize.css'>
+    <link rel='stylesheet' href='css/skeleton.css'>
+    <link rel='stylesheet' href='css/custom.css'>
+</head>
+<body>
+<div class='container'>
+    <section class='header'><h3>Generar código autonumérico con reinicio anual</h3></section>
+    <p><a href='index.php?p=factura_crear'>Crear factura</a></p>
 
-$query = $dbh->prepare("SELECT * FROM factura;");
-// Establece la forma de devolver los resultados, en este caso devolverá un array asociativo
-$query->setFetchMode(PDO::FETCH_ASSOC);
-$query->execute();
-echo '<ul>';
-while ($row = $query->fetch()){
-    echo '<li>' . $row["codigo"] . '</li>';
-}
-echo '</ul>';
+    <?php
+    $query = $dbh->prepare("SELECT * FROM factura;");
+    // Establece la forma de devolver los resultados, en este caso devolverá un array asociativo
+    $query->setFetchMode(PDO::FETCH_ASSOC);
+    $query->execute();
+    echo '<ul>';
+    while ($row = $query->fetch()) {
+        echo '<li>' . $row["codigo"] . '</li>';
+    }
+    echo '</ul>';
+    ?>
 
-echo "</body></html>";
+</div>
+</body>
+</html>
